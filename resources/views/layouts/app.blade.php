d4dfdd93bb1ec2234dd8d1d95053d94c3b0d9568
unknown <ahmedfadhel67@gmail.com>
2019-08-07 19:22:40 +0300

    remove floating ad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,10 +104,6 @@
     <div class="container ads mt-4" >
       <div id="juicy-ads" class="leaderboard "></div>
     </div>
-    <!-- Start JuicyAds Float Ad -->
-    <script type="text/javascript">juicy_adzone = '783221';</script>
-    <script type="text/javascript" src="https://adserver.juicyads.com/js/jfc.js" charset="utf-8"></script>
-    <!-- End JuicyAds Float Ad -->
     {{-- PopCash script code --}}
     <script type="text/javascript">
       var wid = '504629';
